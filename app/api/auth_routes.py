e556381f15a2b2d325744a15379d56361eefadee
John McElroy <mcelrj@gmail.com>
2024-07-18 22:00:15 -0500

    auth routes updated to use front end input

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 from flask_login import current_user, login_user, logout_user#, login_required
 from app.models import User, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
+# from app.forms import LoginForm
+# from app.forms import SignUpForm
+from .utils import validate_MustStr
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -20,17 +21,43 @@ def authenticate():
 def login():
     """
     Logs a user in
+    body:
+        credential
+        password
     """
-    form = LoginForm()
+    body = request.json
+    errors = {}
+    validate_MustStr('credential', body, errors)
+    validate_MustStr('password', body, errors)
+
+    if errors:
+        return {"errors": errors}, 400 #if errors else {}
+
+    # credential can be email or username
+
+    password = body['password']
+    credential = body['credential']
+    user = User.query.filter(User.email == credential or User.username == credential).first()
+
+    if not user:
+        return {"errors": {'user': 'No such user exists.'}}, 400
+
+    if not user.check_password(password):
+        return {"errors": {'password': 'Password was incorrect.'}}, 400
+
+    login_user(user)
+    return user.to_dict()
+
+    # form = LoginForm()
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
+    # form['csrf_token'].data = request.cookies['csrf_token']
+    # if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
-        login_user(user)
-        return user.to_dict()
-    return form.errors, 401
+    #     user = User.query.filter(User.email == form.data['email']).first()
+    #     login_user(user)
+    #     return user.to_dict()
+    # return form.errors, 401
 
 
 @auth_routes.route('/logout')
@@ -46,20 +73,53 @@ def logout():
 def sign_up():
     """
     Creates a new user and logs them in
+    body:
+        first_name
+        last_name
+        username
+        email
+        password
+        profile_image
     """
-    form = SignUpForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        user = User(
-            username=form.data['username'],
-            email=form.data['email'],
-            password=form.data['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        return user.to_dict()
-    return form.errors, 401
+    body = request.json
+    errors = {}
+    validate_MustStr('first_name', body, errors)
+    validate_MustStr('last_name', body, errors)
+    validate_MustStr('username', body, errors)
+    validate_MustStr('email', body, errors)
+    validate_MustStr('password', body, errors)
+    validate_MustStr('profile_image', body, errors)
+
+    if errors:
+        return {"errors": errors}, 400 #if errors else {}
+
+    user = User(
+        first_name=body['first_name'],
+        last_name=body['last_name'],
+        username=body['username'],
+        email=body['email'],
+        password=body['password'], #setting password creats a hashed pw.
+        profile_image=body['profile_image']
+    )
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    return user.to_dict()
+
+
+    # form = SignUpForm()
+    # form['csrf_token'].data = request.cookies['csrf_token']
+    # if form.validate_on_submit():
+    #     user = User(
+    #         username=form.data['username'],
+    #         email=form.data['email'],
+    #         password=form.data['password']
+    #     )
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     login_user(user)
+    #     return user.to_dict()
+    # return form.errors, 401
 
 
 @auth_routes.route('/unauthorized')
