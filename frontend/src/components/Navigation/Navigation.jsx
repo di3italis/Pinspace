38152e428a93281c6fa350645f81e4e48dd4244e
takashi <takashi.sh@proton.me>
2024-07-21 23:44:25 -0700

    tested merges from frontend and FEboards, added conditional link to My Boards

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import Pins from "../Pins";
 import "./Navigation.css";
 
 function Navigation() {
+    const sessionUser = useSelector((state) => state.session.user);
+
   return (
     <ul>
       <li>
@@ -12,6 +16,13 @@ function Navigation() {
       <li>
         <ProfileButton />
       </li>
+      <li>
+        <NavLink to="/pins/all/">Pins</NavLink>
+      </li>
+      {sessionUser && 
+      <li>
+        <NavLink to="/boards/">My Boards</NavLink>
+      </li>}
     </ul>
   );
 }
