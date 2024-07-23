799139e9e0c8c73960e39933d3e6e2e0085dfa44
takashi <takashi.sh@proton.me>
2024-07-22 22:10:46 -0700

    replaced upgrade and seed in docker

--- frontend/src/components/Pins/Pins.jsx
+++ frontend/src/components/Pins/Pins.jsx
@@ -7,7 +7,7 @@ import styles from "./Pins.module.css";
 
 export default function Pins() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
+    // const sessionUser = useSelector((state) => state.session.user);
     // should i use Object.values or just grab state.pins?
     const pins = useSelector((state) => Object.values(state.pins));
     // const pins = useSelector((state) => state.pins);
@@ -30,9 +30,8 @@ export default function Pins() {
     return (
         <div className={styles.pins}>
             {pins.map((pin) => (
-                <PinCard key={pin.id} pin={pin} addBoard={true}/>
+                <PinCard key={pin.id} pin={pin} addBoard={true} />
             ))}
         </div>
     );
 }
-
